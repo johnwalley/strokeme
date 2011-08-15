e1d08fe2fa5f915f4b16ad6c389b1d24c709232b
John Walley <john@walley.org.uk>
2011-08-15 20:47:07 +0100

    Initial commit

---
+++ strokeme/src/org/walley/strokeme/StrokeMeActivity.java
@@ -0,0 +1,13 @@
+package org.walley.strokeme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StrokeMeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
