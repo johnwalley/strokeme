0256e1df1822e8584fba5ef4c2d6faef273015cd
John Walley <john@walley.org.uk>
2011-09-03 20:57:29 +0100

    Minor improvements.
    History time now reflects last stroke time rather than current time.

--- strokeme/src/org/walley/strokeme/StrokeMeActivity.java
+++ strokeme/src/org/walley/strokeme/StrokeMeActivity.java
@@ -4,20 +4,25 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import jama.Matrix;
 import jkalman.JKalman;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -28,6 +33,7 @@ public class StrokeMeActivity extends Activity implements OnClickListener {
     private static final double resetPeriod = 3.0;
     private static final int maxHistory = 3;
 	
+    private LinearLayout layoutView;
 	private TextView rateView;
 	private ListView historyView;
 	
@@ -40,10 +46,11 @@ public class StrokeMeActivity extends Activity implements OnClickListener {
 	
 	double strokeRate; // The current estimate of the stroke rate
 	
-	private List<String> history; // Array holding history of most recent stroke rates
-	private ArrayAdapter<String> historyAdapter;
+	private List<HistoryItem> history; // Array holding history of most recent stroke rates
+	private ArrayAdapter<HistoryItem> historyAdapter;
 	
-	private long lastTime; // time of last stroke
+	private long lastTime; // time of last stroke (in milliseconds)
+	private Date lastDate; // time of last stroke (used for history)
 	
 	private boolean displayHistory;
 
@@ -55,13 +62,40 @@ public class StrokeMeActivity extends Activity implements OnClickListener {
         setContentView(R.layout.main);
         
         rateView = (TextView)findViewById(R.id.rate);
-        rateView.setOnClickListener(this);
+        
+        layoutView = (LinearLayout)findViewById(R.id.screen);
+
+        layoutView.setOnClickListener(this);
         
         historyView = (ListView)findViewById(R.id.history);
-        history = new ArrayList<String>();
-        historyAdapter = new ArrayAdapter<String>(this, R.layout.history_item, history);
+        history = new ArrayList<HistoryItem>();
+                
+        // Define custom ArrayAdapter and override getView
+        historyAdapter = new ArrayAdapter<HistoryItem>(this, R.layout.history_item, history) {
+        	@Override
+        	public View getView(int position, View convertView, ViewGroup parent) {
+        		
+        		View row;
+         
+        		if (null == convertView) {
+        			LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        			row = vi.inflate(R.layout.history_item, null);
+        		} else {
+        			row = convertView;
+        		}
+         
+        		TextView rateHistoryView = (TextView) row.findViewById(R.id.rateHistory);
+        		rateHistoryView.setText(getItem(position).getRate());
+        		
+        		TextView timeHistoryView = (TextView) row.findViewById(R.id.timeHistory);
+        		timeHistoryView.setText(getItem(position).getTime());
+         
+        		return row;
+        	}
+        };
+
         historyView.setAdapter(historyAdapter);
-        
+                
         displayHistory = true;
         
         numStrokes = 0; // Just started. No strokes recorded yet
@@ -71,6 +105,7 @@ public class StrokeMeActivity extends Activity implements OnClickListener {
         // Instantiate Kalman filter implementation
         try {
 			kalman = new JKalman(2, 1);
+			
             kalman.setProcess_noise_cov(kalman.getProcess_noise_cov().identity());
             kalman.setMeasurement_noise_cov(kalman.getMeasurement_noise_cov().identity());	
 		} catch (Exception e) {
@@ -81,6 +116,31 @@ public class StrokeMeActivity extends Activity implements OnClickListener {
         measurement = new Matrix(1, 1); // measurement [z]
     }
     
+    private class HistoryItem {
+    	private String rate;
+    	private String time;
+    	
+    	public HistoryItem(String rate, String time) {
+    		this.setRate(rate);
+    		this.setTime(time);
+    	}
+
+		public String getTime() {
+			return time;
+		}
+
+		public void setTime(String time) {
+			this.time = time;
+		}
+
+		public String getRate() {
+			return rate;
+		}
+
+		public void setRate(String rate) {
+			this.rate = rate;
+		}
+    }
     
     // Tap received
     public void onClick(View v) {   	
@@ -91,20 +151,22 @@ public class StrokeMeActivity extends Activity implements OnClickListener {
     	double dt = (double)(currentTime-lastTime)/1000.0;
     	
     	// If sufficient time has passed we treat this stroke as the first in a new sequence
-    	if (dt>resetPeriod & numStrokes>2) {
-    		numStrokes = 1;
-    		
-    		DateFormat formatter = new SimpleDateFormat("h:mm a");
-    		Calendar calendar = Calendar.getInstance();
-
-    		formatter.format(calendar.getTime());    		
-    		
-    		// Really should be a bounded Queue but this works
-    		historyAdapter.insert(String.format("%2.0f - %s", strokeRate, formatter.format(calendar.getTime())), 0); // Insert at beginning of list
-    		if (historyAdapter.getCount()>maxHistory) {
-    			historyAdapter.remove(historyAdapter.getItem(maxHistory-1)); // Remove oldest history item if over limit
+    	if (dt>resetPeriod) {
+    		    		
+    		// No point in storing in history if numStrokes equals 2
+    		if (numStrokes>2) {
+	    		// Move this value to history    		
+	    		DateFormat formatter = new SimpleDateFormat("h:mm a");
+	    		Calendar calendar = Calendar.getInstance();
+	
+	    		// Really should be a bounded Queue but this works
+	    		historyAdapter.insert(new HistoryItem(String.format("%2.0f spm", strokeRate), String.format("%s", formatter.format(lastDate))), 0); // Insert at beginning of list
+	    		if (historyAdapter.getCount()>maxHistory) {
+	    			historyAdapter.remove(historyAdapter.getItem(maxHistory-1)); // Remove oldest history item if over limit
+	    		}
     		}
     		
+    		numStrokes = 1;
     	}
     	
     	switch (numStrokes) {
@@ -142,6 +204,7 @@ public class StrokeMeActivity extends Activity implements OnClickListener {
     	}
     	
     	lastTime = currentTime;
+    	lastDate = calendar.getTime();
     	
     }
     
